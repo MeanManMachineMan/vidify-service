18198133d83533875f93f6b958f8abde8379b128
Vidur <vidur.p@hashedin.com>
2021-12-11 00:30:20 +0530

    Initialized vidify-service! :)

---
+++ src/main/java/com/vidify/VidifyApplication.java
@@ -0,0 +1,13 @@
+package com.vidify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidify/VidifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
